9677a8c44fa811fa815ddb401b1d528616bedcc6
The Arctic Fox <thearcticfox.old@gmail.com>
2024-11-30 14:28:46 +0300

    Updating to 1.1.0.1.ο

---
+++ advancedserver/entities/HillThunder.c
@@ -0,0 +1,30 @@
+#include <entities/HillThunder.h>
+#include <CMath.h>
+
+bool thunder_tick(Server* server, Entity* entity)
+{
+	Thunder* th = (Thunder*)entity;
+
+	if (th->timer <= 2 * TICKSPERSEC && !th->flag)
+	{
+		Packet pack;
+		PacketCreate(&pack, SERVER_GHZTHUNDER_STATE);
+		PacketWrite(&pack, packet_write8, 0);
+		server_broadcast(server, &pack, true);
+		th->flag = true;
+	}
+	else if (th->timer <= 0)
+	{
+		Packet pack;
+		PacketCreate(&pack, SERVER_GHZTHUNDER_STATE);
+		PacketWrite(&pack, packet_write8, 1);
+		server_broadcast(server, &pack, true);
+
+        th->timer = (g_config.thunder_timer + (!g_config.thunder_timer_offset ? 0 : (rand() % g_config.thunder_timer_offset))) * TICKSPERSEC;
+		th->flag = false;
+		return true;
+	}
+
+	th->timer -= server->delta;
+	return true;
+}
